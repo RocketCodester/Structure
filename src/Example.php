7f6df4f7a3e732a0b5bcc816415e333a348cad43
Rocket Codester <45156318+RocketCodester@users.noreply.github.com>
2018-12-23 20:46:09 -0600

    Initial Commit

---
+++ src/Example.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+namespace Structure\Example;
+
+class Example
+{
+    public function getSomething()
+    {
+        return 'something interesting was loaded here';
+    }
+}
